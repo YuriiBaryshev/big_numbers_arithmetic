a9f11a7ec3c11934e5bcc0716132e2d8de8c4fff
YuriiBaryshev <yura.baryshev@gmail.com>
2023-05-06 20:38:55 +0300

    create new dart package project

---
+++ example/big_numbers_arithmetic_example.dart
@@ -0,0 +1,6 @@
+import 'package:big_numbers_arithmetic/big_numbers_arithmetic.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/big_numbers_arithmetic.dart
@@ -0,0 +1,6 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library big_numbers_arithmetic;
+
+export 'src/big_numbers_arithmetic_base.dart';
