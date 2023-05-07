a726c227cc5e2eb435e5b78a356c0d7b18acb006
YuriiBaryshev <yura.baryshev@gmail.com>
2023-05-07 19:18:26 +0300

    refactor style

--- lib/src/big_number_factory_method.dart
+++ lib/src/big_number_factory_method.dart
@@ -13,17 +13,20 @@ class BigNumberFactoryMethod {
         " so the proper way of their usage is to call them directly");
   }
 
+
   ///Creates an instance of 32 bit based BigNumber for x86 or any other 32 bit
   ///based platforms
   static createBigNumberX86(int maxBitLength) {
     throw UnimplementedError("BigNumberFactoryMethod: it hasn't been implemented yet");
   }
 
+
   ///Creates an instance of 64 bit based BigNumber for x64 or any other 64 bit
   ///based platform
   static createBigNumberX64(int maxBitLength) {
     throw UnimplementedError("BigNumberFactoryMethod: it hasn't been implemented yet");
   }
+
 
   ///Creates an instance BigNumber optimized for @platform argument
   static createBigNumber(Platforms platform, int maxBitLength) {
