2d3c36b6466e73f17491b774c68da517913b7791
YuriiBaryshev <yura.baryshev@gmail.com>
2023-05-07 20:50:42 +0300

    create hex string prototype

--- lib/src/big_number.dart
+++ lib/src/big_number.dart
@@ -11,4 +11,33 @@ abstract class BigNumber {
 
   ///platform bits length
   late int _platform;
+
+  ///setting data by hexadecimal string
+  void setHex(String hexString) {
+    //strip '0x' hexadecimal notation if applicable
+    if ((hexString.length >= 2) && (hexString.substring(0, 2) == "0x")) {
+      hexString = hexString.substring(2);
+    }
+
+    //validate input
+    RegExp hexAlphabet = RegExp(r"^[a-f0-9]", caseSensitive: false);
+    if (!hexAlphabet.hasMatch(hexString)) {
+      throw ArgumentError("BigNumber: cannot convert hex string into BigNumber "
+          "platform specific implementation due to restricted symbols for hexadecimal"
+          " number encoding alphabet within inputted string");
+    }
+
+    //check whenever inputted string is fitting variable's length
+    if (hexString.length > (_maxBitLength >> 2)) {
+      throw ArgumentError("BigNumber: provided hex string is longer then "
+          "this variable's maxBitLength ($_maxBitLength). Try to create another "
+          "instance of class with at least ${hexString.length << 2} maxBitLength");
+    }
+
+    //platform specific conversion
+    _dataFromHex(hexString);
+  }
+
+  ///converting hex string into platform specific data array
+  void _dataFromHex(String hexString);
 }
