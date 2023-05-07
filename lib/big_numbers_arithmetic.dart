f9bd5f3c9983faefc47ca25aaf587be15ddd174f
YuriiBaryshev <yura.baryshev@gmail.com>
2023-05-07 20:22:44 +0300

    add library structure

--- lib/big_numbers_arithmetic.dart
+++ lib/big_numbers_arithmetic.dart
@@ -1,6 +1,12 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+/// Implements big numbers arithmetic.
 library big_numbers_arithmetic;
 
-export 'src/big_number_factory_method.dart';
+import 'dart:typed_data';
+
+part 'src/big_number.dart';
+part 'src/big_number_x64.dart';
+part 'src/big_number_x86.dart';
+part 'src/big_number_factory_method.dart';
+
+
+
