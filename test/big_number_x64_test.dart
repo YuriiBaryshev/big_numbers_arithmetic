375e9ebb2232397868524bbbe756135d49692830
YuriiBaryshev <yura.baryshev@gmail.com>
2023-05-09 22:48:15 +0300

    refactor names

--- test/big_number_x64_test.dart
+++ test/big_number_x64_test.dart
@@ -4,17 +4,31 @@ import 'package:test/test.dart';
 void main() {
   group('BigNumberX64:', () {
     test('sets and gets hex from 1 element array', () {
-      BigNumberX64 bnx64 = BigNumberX64(64);
-      bnx64.setHex("0x1234567812345678");
-      expect(bnx64.getHex(), "0x1234567812345678");
+      BigNumberX64 bn = BigNumberX64(64);
+      bn.setHex("0x1234567812345678");
+      expect(bn.getHex(), "0x1234567812345678");
+
+      bn.setHex("1234");
+      expect(bn.getHex(), "0x0000000000001234");
+      expect(bn.getHex(hasLeadingZeroes: false), "0x1234");
+
+      bn.setHex("12340");
+      expect(bn.getHex(), "0x0000000000012340");
+      expect(bn.getHex(hasLeadingZeroes: false), "0x12340");
+    });
+
 
       bnx64.setHex("1234");
       expect(bnx64.getHex(), "0x0000000000001234");
       expect(bnx64.getHex(hasLeadingZeroes: false), "0x1234");
 
-      bnx64.setHex("12340");
-      expect(bnx64.getHex(), "0x0000000000012340");
-      expect(bnx64.getHex(hasLeadingZeroes: false), "0x12340");
+      bn.setHex("1234");
+      expect(bn.getHex(), "0x00000000000000000000000000001234");
+      expect(bn.getHex(hasLeadingZeroes: false), "0x1234");
+
+      bn.setHex("12340");
+      expect(bn.getHex(), "0x00000000000000000000000000012340");
+      expect(bn.getHex(hasLeadingZeroes: false), "0x12340");
     });
   });
 }
--- test/big_number_x86_test.dart
+++ test/big_number_x86_test.dart
@@ -16,5 +16,20 @@ void main() {
       expect(bnx64.getHex(), "0x00012340");
       expect(bnx64.getHex(hasLeadingZeroes: false), "0x12340");
     });
+
+
+    test('sets and gets hex from 2 element array', () {
+      BigNumberX86 bn = BigNumberX86(64);
+      bn.setHex("0x123456781234567809abcdef09abcdef");
+      expect(bn.getHex(), "0x123456781234567809abcdef09abcdef");
+
+      bn.setHex("1234");
+      expect(bn.getHex(), "0x00000000000000000000000000001234");
+      expect(bn.getHex(hasLeadingZeroes: false), "0x1234");
+
+      bn.setHex("12340");
+      expect(bn.getHex(), "0x00000000000000000000000000012340");
+      expect(bn.getHex(hasLeadingZeroes: false), "0x12340");
+    });
   });
 }
