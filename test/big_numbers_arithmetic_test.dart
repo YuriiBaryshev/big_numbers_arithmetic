a3796a32862153e771ceabe7bd1b3550fc31aaa2
YuriiBaryshev <yura.baryshev@gmail.com>
2023-05-09 23:55:49 +0300

    add tests for constructors

--- test/big_numbers_arithmetic_test.dart
+++ test/big_numbers_arithmetic_test.dart
@@ -4,12 +4,78 @@ import 'package:test/test.dart';
 void main() {
   group('A group of tests', () {
 
-    setUp(() {
-      // Additional setup goes here.
+    test('fails to fit big value into short variable', ()
+    {
+      BigNumber bn;
+      expect(() {
+        bn = BigNumberX64(64);
+        bn.setHex("1234567890abcdef1");
+      },
+          throwsArgumentError
+      );
+
+      expect(() {
+        bn = BigNumberX86(64);
+        bn.setHex("1234567890abcdef1");
+      },
+          throwsArgumentError
+      );
     });
 
-    test('First Test', () {
 
+    test("fails to create variables of length not dividable by platform's length", ()
+    {
+      BigNumber bn;
+      expect(() {
+        bn = BigNumberX64(42); //throws here
+        bn.getHex(); //just in order to mute IDE's warning of unused variable
+      },
+          throwsArgumentError
+      );
+
+      expect(() {
+        bn = BigNumberX86(42); //throws here
+        bn.getHex();  //just in order to mute IDE's warning of unused variable
+      },
+          throwsArgumentError
+      );
+    });
+
+
+    test("fails to create variables of negative length", ()
+    {
+      BigNumber bn;
+      expect(() {
+        bn = BigNumberX64(-42); //throws here
+        bn.getHex(); //just in order to mute IDE's warning of unused variable
+      },
+          throwsArgumentError
+      );
+
+      expect(() {
+        bn = BigNumberX86(-42); //throws here
+        bn.getHex();  //just in order to mute IDE's warning of unused variable
+      },
+          throwsArgumentError
+      );
+    });
+
+    test("fails to create variables of zero length", ()
+    {
+      BigNumber bn;
+      expect(() {
+        bn = BigNumberX64(0); //throws here
+        bn.getHex(); //just in order to mute IDE's warning of unused variable
+      },
+          throwsArgumentError
+      );
+
+      expect(() {
+        bn = BigNumberX86(0); //throws here
+        bn.getHex();  //just in order to mute IDE's warning of unused variable
+      },
+          throwsArgumentError
+      );
     });
   });
 }
