6f77e1fb4f312ffc9390ea8b6bcc69051b4c319f
YuriiBaryshev <yura.baryshev@gmail.com>
2023-05-10 12:33:39 +0300

    add test cases for shiftRight

--- test/big_numbers_arithmetic_test.dart
+++ test/big_numbers_arithmetic_test.dart
@@ -79,10 +79,8 @@ void main() {
   group("Bitwise operations of", () {
     for (String implementation in ["BigNumberX86", "BigNumberX64"]) {
       group(implementation, () {
-        late BigNumber bn1;
-        late BigNumber bn2;
-        late BigNumber zero;
-        late BigNumber allOnes;
+        late BigNumber bn1, bn2, zero, allOnes, allA, all5,
+            allOnesShifted1PositionRight, allOnesShifted42PositionRight;
 
         setUp(() {
           switch (implementation) {
@@ -92,6 +90,10 @@ void main() {
                 bn2 = BigNumberX86(128);
                 zero = BigNumberX86(128);
                 allOnes = BigNumberX86(128);
+                allA = BigNumberX86(128);
+                all5 = BigNumberX86(128);
+                allOnesShifted1PositionRight = BigNumberX86(128);
+                allOnesShifted42PositionRight = BigNumberX86(128);
                 break;
               }
             case "BigNumberX64" :
@@ -100,6 +102,10 @@ void main() {
                 bn2 = BigNumberX64(128);
                 zero = BigNumberX64(128);
                 allOnes = BigNumberX64(128);
+                allA = BigNumberX64(128);
+                all5 = BigNumberX64(128);
+                allOnesShifted1PositionRight = BigNumberX64(128);
+                allOnesShifted42PositionRight = BigNumberX64(128);
                 break;
               }
             default:
@@ -113,6 +119,12 @@ void main() {
 
           zero.setHex("0");
           allOnes.setHex("ffffffffffffffffffffffffffffffff");
+
+          allA.setHex("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+          all5.setHex("55555555555555555555555555555555");
+
+          allOnesShifted1PositionRight.setHex("7fffffffffffffffffffffffffffffff");
+          allOnesShifted42PositionRight.setHex("00000000003fffffffffffffffffffff");
         });
 
 
@@ -154,6 +166,21 @@ void main() {
           expect(bn1 & bn1, bn1, reason: "$bn1 & $bn1 is not equal $bn1");
           expect(bn2 & bn2, bn2, reason: "$bn2 & $bn2 is not equal $bn2");
           expect(bn1 & bn2, zero, reason: "$bn1 & $bn2 is not equal $zero");
+        });
+
+
+        test("shift right for 128 bit", () {
+          expect(bn1 >> 1, zero, reason: "$bn1 shifted right 1 position is not equal $zero");
+          expect(allOnes >> 1, allOnesShifted1PositionRight, reason: "$allOnes shifted right 1"
+              " position is not equal $allOnesShifted1PositionRight");
+          expect(allOnes >> 42, allOnesShifted42PositionRight, reason: "$allOnes shifted right 1"
+              " position is not equal $allOnesShifted42PositionRight");
+          expect(allOnes >> 127, bn1, reason: "$allOnes shifted right 127"
+              " position is not equal $bn1");
+          expect(allA >> 1, all5, reason: "$allA shifted right 1"
+              " position is not equal $all5");
+          expect(allA >> 71, all5 >> 70, reason: "$allA shifted right 71"
+              " position is not equal $all5 shifted right 70 positions");
         });
       });
     }
